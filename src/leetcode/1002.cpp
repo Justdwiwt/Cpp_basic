9418b153bdf137044cab1f64a6a07a0070ed0379
Justdwiwt <len_master@hotmail.com>
2019-09-28 10:20:48 +0800

    leetCode 1002

--- src/leetcode/1002.cpp
+++ src/leetcode/1002.cpp
@@ -8,15 +8,15 @@ public:
     vector<string> commonChars(vector<string> &A) {
         vector<string> ans;
         int count[26];
-        for (int i = 0; i < 26; ++i) {
-            count[i] = 101;
+        for (int &i : count) {
+            i = 101;
         }
-        for (int i = 0; i < A.size(); i++) {
+        for (auto &i : A) {
             int word[26] = {0};
-            for (int j = 0; j < A[i].size(); j++)
-                word[(int) A[i][j] - 'a']++;
-            for (int i = 0; i < 26; i++)
-                count[i] = min(count[i], word[i]);
+            for (char j : i)
+                word[(int) j - 'a']++;
+            for (auto k = 0; k < 26; k++)
+                count[k] = min(count[k], word[k]);
         }
         for (int i = 0; i < 26; i++)
             if (count[i])
