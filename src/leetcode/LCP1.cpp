5c59bf14c5ca4cfadd0be4efbb5bb680d0341e7d
Justdwiwt <len_master@hotmail.com>
2019-09-28 14:16:13 +0800

    LCP 1

---
+++ src/leetcode/LCP1.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int game(vector<int> &guess, vector<int> &answer) {
+        int res = 0;
+        for (int i = 0; i < guess.size(); ++i)
+            if (guess[i] == answer[i]) res++;
+        return res;
+    }
+};
