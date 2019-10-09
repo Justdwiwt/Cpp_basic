57ecd03375b929091516cb95b495f4105104eb23
Justdwiwt <len_master@hotmail.com>
2019-10-09 14:32:40 +0800

    leetCode 189

---
+++ src/leetcode/189.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int> &nums, int k) {
+        k = k % nums.size();
+        int iLen = nums.size();
+
+        while (k > 0 && iLen > 1 && iLen != k) {
+            for (int i = iLen - 1; i > k - 1; i--)
+                swap(nums[i], nums[i - k]);
+            int tmp = k;
+            k = k - iLen % k;
+            iLen = tmp;
+        }
+    }
+};
